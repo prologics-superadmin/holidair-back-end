99b0af3ebcaef1deee48e29b066910231470a2f4
NAYANA-PC\nayana <nayanadarshana1@gmail.com>
2024-07-04 19:24:58 +0530

    updates

--- src/services/BookingService.js
+++ src/services/BookingService.js
@@ -22,8 +22,10 @@ class BookingService {
 
       if (filters.user && filters.user !== "") {
         // Step 1: Find users with the matching user_name
-        const users = await User.find({ user_name: { $regex: filters.user, $options: "i" } });
-        const userIds = users.map(user => user._id);
+        const users = await User.find({
+          user_name: { $regex: filters.user, $options: "i" },
+        });
+        const userIds = users.map((user) => user._id);
 
         // Step 2: Add user_id filter to the query
         query.user_id = { $in: userIds };
@@ -36,18 +38,22 @@ class BookingService {
           select: "user_name", // Select only name field of the brand object
         });
       const count = await BookingDetails.countDocuments(query);
-      const dataResponse = await Promise.all(flightBooking.map(async (booking, index) => {
-        return {
-          id: booking.booking_id,
-          customer_name: booking.user_id.user_name,
-          email: booking.email,
-          provider_reference: booking.brightsun_reference ? booking.brightsun_reference : "",
-          trip_type: booking.trip_type,
-          amount: booking.amount,
-          contact_number: booking.contact_number,
-          booking_status: booking.booking_status,
-        };
-      }));
+      const dataResponse = await Promise.all(
+        flightBooking.map(async (booking, index) => {
+          return {
+            id: booking.booking_id,
+            customer_name: booking.user_id ? booking.user_id.user_name : "",
+            email: booking.email,
+            provider_reference: booking.brightsun_reference
+              ? booking.brightsun_reference
+              : "",
+            trip_type: booking.trip_type,
+            amount: booking.amount,
+            contact_number: booking.contact_number,
+            booking_status: booking.booking_status,
+          };
+        })
+      );
 
       let response;
 
@@ -71,7 +77,6 @@ class BookingService {
       // console.log(response)
 
       return response;
-
     } catch (error) {
       throw error;
     }
@@ -94,26 +99,25 @@ class BookingService {
         }
       }
 
-
       const hotelBookings = await HotelBookingDetail.find(query)
         .skip(skip)
         .limit(itemsPerPage);
       const count = await HotelBookingDetail.countDocuments(query);
 
-
       const dataResponse = hotelBookings.map((booking, index) => {
         return {
           id: booking.booking_id,
-          customer_name: booking.name + ' ' + booking.surname,
+          customer_name: booking.name + " " + booking.surname,
           email: booking.email,
           provider_reference: booking.reference ? booking.reference : "",
           trip_type: booking.trip_type,
           hotel_name: booking.hotel_data ? booking.hotel_data.hotel.name : "",
           amount: booking.hotel_data ? booking.hotel_data.totalSellingRate : "",
-          booking_date: booking.hotel_data ? booking.hotel_data.hotel.checkIn : "",
+          booking_date: booking.hotel_data
+            ? booking.hotel_data.hotel.checkIn
+            : "",
           booking_status: booking.booking_status,
           createdAt: booking.createdAt,
-
         };
       });
 
