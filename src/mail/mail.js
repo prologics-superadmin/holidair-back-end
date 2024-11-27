ae73c2e58d73f4869c5f882366a497813500ecd1
NAYANA-PC\nayana <nayanadarshana1@gmail.com>
2024-11-27 22:39:11 +0530

    updates

--- src/mail/mail.js
+++ src/mail/mail.js
@@ -5,8 +5,8 @@ const transporter = nodemailer.createTransport({
   port: 587, // Using TLS on port 587
   secure: false, // Use `false` for TLS
   auth: {
-    user: process.env.SENDGRID_USER_NAME,
-    pass: process.env.SENDGRID_PASSWORD,
+    user: "apikey",
+    pass: "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",
   },
   tls: {
     rejectUnauthorized: false, // Optional, allows self-signed certificates if needed
